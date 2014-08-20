f6a7cef790579dcb90e79d1da9dabb41f6d5ab90
Marius Vollmer <mvollmer@redhat.com>
2014-08-20 14:29:42 +0300

    shell: Bond slaves are "members", Bridge slaves are "ports".
    Some are even Gold Members.

--- modules/shell/cockpit-networking.js
+++ modules/shell/cockpit-networking.js
@@ -2037,6 +2037,9 @@ PageNetworkInterface.prototype = {
                 return;
             }
 
+            $('#network-interface-slaves thead th:first-child').
+                text(con.Settings.connection.type == "bond"? _("Members") : _("Ports"));
+
             con.Slaves.forEach(function (slave_con) {
                 slave_con.Interfaces.forEach(function(iface) {
                     var dev = iface.Device;
@@ -2655,7 +2658,7 @@ PageNetworkBridgeSettings.prototype = {
                                 settings.connection.interface_name = val;
                             }))),
                 $('<tr>').append(
-                    $('<td class="top">').text(_("Members")),
+                    $('<td class="top">').text(_("Ports")),
                     $('<td>').append(render_slave_interface_choices(model,
                                                                     PageNetworkBridgeSettings.connection))).
                     toggle(!PageNetworkBridgeSettings.connection),
