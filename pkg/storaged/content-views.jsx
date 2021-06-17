aceb74bee74b19431a1ec692ddb7f756f9955b4a
Garrett LeSage <garrett@lesage.us>
2021-06-17 16:10:46 +0200

    storage: Move level to tr and offset expander

--- pkg/storaged/content-views.jsx
+++ pkg/storaged/content-views.jsx
@@ -426,7 +426,7 @@ function append_row(client, rows, level, key, name, desc, tabs, job_object) {
     var cols = [
         {
             title: (
-                <span key={name} className={"content-level-" + level}>
+                <span key={name}>
                     {utils.format_size_and_text(desc.size, desc.text)}
                     {info}
                 </span>)
@@ -440,7 +440,7 @@ function append_row(client, rows, level, key, name, desc, tabs, job_object) {
     }
 
     rows.push({
-        props: { key },
+        props: { key, className: "content-level-" + level },
         columns: cols,
         expandedContent: <ListingPanel tabRenderers={tabs.renderers} />
     });
