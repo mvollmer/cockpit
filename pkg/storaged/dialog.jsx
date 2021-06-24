961f98bbf4664be07f61c3794350f5564c69d015
Marius Vollmer <mvollmer@redhat.com>
2021-06-24 11:17:17 +0300

    storage: Combine crypto and fsys rows

--- pkg/storaged/dialog.jsx
+++ pkg/storaged/dialog.jsx
@@ -519,6 +519,7 @@ export const PassInput = (tag, title, options) => {
         render: (val, change, validated) =>
             <TextInputPF4 data-field={tag} data-field-type="text-input"
                    validated={validated}
+                   disabled={options.disabled}
                    aria-label={title}
                    type="password" value={val}
                    onChange={change} />
@@ -633,6 +634,10 @@ export const SelectRow = (tag, headers, options) => {
     };
 };
 
+function nice_block_name(block) {
+    return block_name(client.blocks[block.CryptoBackingDevice] || block);
+}
+
 export const SelectSpaces = (tag, title, options) => {
     return {
         tag: tag,
@@ -649,7 +654,7 @@ export const SelectSpaces = (tag, title, options) => {
                     data-field={tag} data-field-type="select-spaces">
                     { options.spaces.map(spc => {
                         const selected = (val.indexOf(spc) >= 0);
-                        const block = spc.block ? block_name(spc.block) : "";
+                        const block = spc.block ? nice_block_name(spc.block) : "";
                         const desc = block === spc.desc ? "" : spc.desc;
 
                         const on_change = (checked) => {
@@ -703,7 +708,7 @@ export const SelectSpace = (tag, title, options) => {
                 <DataList isCompact
                     data-field={tag} data-field-type="select-spaces">
                     { options.spaces.map(spc => {
-                        const block = spc.block ? block_name(spc.block) : "";
+                        const block = spc.block ? nice_block_name(spc.block) : "";
                         const desc = block === spc.desc ? "" : spc.desc;
                         const on_change = (event) => {
                             if (event.target.checked)
